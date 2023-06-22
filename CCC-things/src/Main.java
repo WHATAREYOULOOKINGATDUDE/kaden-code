b23b5a2dd1be42842e5ad5d435e899bf7e2df16d
Kaden Zhang <zhangwkaden@gmail.com>
2023-06-22 16:39:40 -0400

    Brute force :)

--- CCC-things/src/Main.java
+++ CCC-things/src/Main.java
@@ -1,43 +1,31 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int name = 0;
-        int highest = 0;
-        int highest2 = 0;
-        int placeholder = 0;
-        int placeholder2 = 0;
-        int size = sc.nextInt();
-        int inputs2 [][] = new int[3][size];
-        String[] inputs = new String[size];
-        for (int i = 0; i < size; i++){
-            inputs[i] = sc.next();
-            for(int j = 0; j < 3; j++) {
-                inputs2[j][i] = sc.nextInt();
+        int n = sc.nextInt();
+        int count = 0;
+        if(n < 6) {
+            for (int i = 0; i <= n / 2; i++) {
+                count++;
             }
+        }
+        else{
+            if(n == 6){
+                count = 3;
+            }
+            else if(n == 7){
+                count = 2;
+            }
+            else if(n == 8){
+                count = 2;
+            }
+            else{
+                count = 1;
+            }
+
+        }
+        System.out.println(count);
         }
 
-        for(int i = 0; i < size; i++){
-            if(2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] > placeholder) {
-                highest = i;
-                placeholder = 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i];
-                }
-        }
-        for(int i = 0; i < size; i++){
-            if(placeholder >= 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] && 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] > placeholder2 && highest != i){
-                highest2 = i;
-                placeholder2 = 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i];
-            }
-        }
-        System.out.println(inputs[highest]);
-        System.out.println(inputs[highest2]);
     }
-}
-/*
-4
-ABC 13 22 1
-DEF 10 20 30
-GHI 11 2 2
-JKL 20 20 20
-*/
+
