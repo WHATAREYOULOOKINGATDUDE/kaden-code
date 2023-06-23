dc522d748aa7e66e3ef4fb3ff663dd30001744df
Kaden Zhang <zhangwkaden@gmail.com>
2023-06-22 21:58:46 -0400

    Name update

--- CCC-things/src/Main13.java
+++ CCC-things/src/Main13.java
@@ -1,43 +1,31 @@
 import java.util.Scanner;
-// https://cccgrader.com/getproblem.php?fid=221084&authcode=8da8ad5f866e4bec2889bf24d4f4bc2d
 public class Main13 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int highest = 0;
-        int highest2 = 0;
-        String highestName = null;
-        String highestName2 = null;
-        int size = sc.nextInt();
-        int specs [][] = new int[3][size];
-        String[] names = new String[size];
-        for (int i = 0; i < size; i++){
-            names[i] = sc.next();
-            for(int j = 0; j < 3; j++) {
-                specs[j][i] = sc.nextInt();
-            }
-            int score = 2 * specs[0][i] + 3 * specs[1][i] + specs[2][i];
-            if (score >= highest) {
-                highest2 = highest;
-                highestName2 = highestName;
-                highest = score;
-                highestName = names[i];
-            } else if (score >= highest2) {
-                highest2 = score;
-                highestName2 = names[i];
+        int n = sc.nextInt();
+        int count = 0;
+        if(n < 6) {
+            for (int i = 0; i <= n / 2; i++) {
+                count++;
             }
         }
-        if(highestName != null) {
-            System.out.println(highestName);
+        else{
+            if(n == 6){
+                count = 3;
+            }
+            else if(n == 7){
+                count = 2;
+            }
+            else if(n == 8){
+                count = 2;
+            }
+            else{
+                count = 1;
+            }
+
         }
-        if(highestName2 != null) {
-            System.out.println(highestName2);
+        System.out.println(count);
         }
+
     }
-}
-/*
-4
-ABC 13 22 1
-DEF 10 20 30
-GHI 11 2 2
-JKL 20 20 20
-*/
+
