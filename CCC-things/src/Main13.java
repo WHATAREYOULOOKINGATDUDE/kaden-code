e72bc24dee691e7b6db0bb55f67dda92fd3a0050
Peng Zhang <lekyzhang@yahoo.com>
2023-06-22 21:41:33 -0400

    ccc

--- CCC-things/src/Main13.java
+++ CCC-things/src/Main13.java
@@ -1,37 +1,36 @@
 import java.util.Scanner;
-
+// https://cccgrader.com/getproblem.php?fid=221084&authcode=8da8ad5f866e4bec2889bf24d4f4bc2d
 public class Main13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int name = 0;
         int highest = 0;
         int highest2 = 0;
         int placeholder = 0;
         int placeholder2 = 0;
         int size = sc.nextInt();
-        int inputs2 [][] = new int[3][size];
-        String[] inputs = new String[size];
+        int specs [][] = new int[3][size];
+        String[] names = new String[size];
         for (int i = 0; i < size; i++){
-            inputs[i] = sc.next();
+            names[i] = sc.next();
             for(int j = 0; j < 3; j++) {
-                inputs2[j][i] = sc.nextInt();
+                specs[j][i] = sc.nextInt();
             }
         }
 
         for(int i = 0; i < size; i++){
-            if(2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] > placeholder) {
+            if(2 * specs[0][i] + 3 * specs[1][i] + specs[2][i] > placeholder) {
                 highest = i;
-                placeholder = 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i];
+                placeholder = 2 * specs[0][i] + 3 * specs[1][i] + specs[2][i];
                 }
         }
         for(int i = 0; i < size; i++){
-            if(placeholder >= 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] && 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i] > placeholder2 && highest != i){
+            if(placeholder >= 2 * specs[0][i] + 3 * specs[1][i] + specs[2][i] && 2 * specs[0][i] + 3 * specs[1][i] + specs[2][i] > placeholder2 && highest != i){
                 highest2 = i;
-                placeholder2 = 2 * inputs2[0][i] + 3 * inputs2[1][i] + inputs2[2][i];
+                placeholder2 = 2 * specs[0][i] + 3 * specs[1][i] + specs[2][i];
             }
         }
-        System.out.println(inputs[highest]);
-        System.out.println(inputs[highest2]);
+        System.out.println(names[highest]);
+        System.out.println(names[highest2]);
     }
 }
 /*
