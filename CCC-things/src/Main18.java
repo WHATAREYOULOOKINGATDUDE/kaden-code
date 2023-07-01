20a16c34df96202c3916eb7540277cffd5aa3e02
Peng Zhang <lekyzhang@yahoo.com>
2023-06-30 21:23:16 -0400

    new

---
+++ CCC-things/src/Main18.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//https://cccgrader.com/getproblem.php?fid=221007&authcode=29b8d4570578539945eefff14aefd96c
+public class Main18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int points = 100;
+        int points2 = 100;
+
+        for (int i = 0; i < size; i++) {
+            int pointsRoll = sc.nextInt();
+            int pointsRoll2 = sc.nextInt();
+
+            if (pointsRoll < pointsRoll2) {
+                points -= pointsRoll2;
+            } else if (pointsRoll2 < pointsRoll) {
+                points2 -= pointsRoll;
+            }
+        }
+
+        System.out.println(points);
+        System.out.println(points2);
+    }
+}
