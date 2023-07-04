4c3c320bc5e642ea7da0e5ddd64e733b1626ad63
Kaden Zhang <zhangwkaden@gmail.com>
2023-07-04 14:15:33 -0400

    Almost finished code.

--- CCC-things/src/Main.java
+++ CCC-things/src/Main.java
@@ -3,31 +3,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine().trim();
-        int longest = findLongest(word);
-        System.out.println(longest);
-    }
-    public static int findLongest(String word) {
-        int maxLength = 0;
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = word.length() - 1; j >= i; j--) {
-                if (isPalindrome(word, i, j)) {
-                    int length = j - i + 1;
-                    maxLength = Math.max(maxLength, length);
-                    break;
-                }
+        String input = sc.nextLine();
+        String[] placeholder = input.split(":");
+        int hours = Integer.parseInt(placeholder[0]);
+        int minutes = Integer.parseInt(placeholder[1]);
+        float changeAmountH = 0;
+        float changeAmountM = 0;
+        if(hours + changeAmountH >= 7 && hours + changeAmountH < 10){
+            while(hours + changeAmountH < 10){
+                changeAmountH = 10 - hours;
+            }
+            changeAmountM += (4 - changeAmountH) / 2;
+        }
+
+        else if(hours + changeAmountH >= 15 && hours + changeAmountH < 19){
+            while(hours + changeAmountH < 19){
+                changeAmountH = 19 - hours;
+            }
+            changeAmountM += (4 - changeAmountH) / 2;
+        }
+
+        else{
+            changeAmountH = 2;
+        }
+        int outputM = (int) ((minutes + changeAmountM * 60)) % 60;
+        int outputH = (int) ((hours + changeAmountH) % 24);
+        if(outputM == 0 && changeAmountM == 0){
+            if(outputH < 10) {
+                System.out.println("0" + outputH + ":" + "00");
+            }
+            else{
+                System.out.println(outputH + ":" + "00");
             }
         }
-        return maxLength;
-    }
-    public static boolean isPalindrome(String word, int start, int end) {
-        while (start < end) {
-            if (word.charAt(start) != word.charAt(end)) {
-                return false;
+        else {
+            if(outputH < 10) {
+                System.out.println("0" + outputH + ":" + outputM);
             }
-            start++;
-            end--;
+            else{
+                System.out.println(outputH + ":" + outputM);
+            }
         }
-        return true;
+
+
     }
 }
