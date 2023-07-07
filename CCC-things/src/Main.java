ccd5dc0e61247e32823fb46bb7bfed3dfa34b25d
Peng Zhang <lekyzhang@yahoo.com>
2023-07-06 21:49:16 -0400

    new

--- CCC-things/src/Main.java
+++ CCC-things/src/Main.java
@@ -1,28 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+// https://cccgrader.com/getproblem.php?fid=146516&authcode=e79090007d2b282d7ee16a3d355b9a10
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int tCount = 0;
-        int sCount = 0;
-        for (int i = 0; i < size; i++) {
-            String line = sc.nextLine();
-            for (char c : line.toCharArray()) {
-                if (c == 't' || c == 'T') {
-                    tCount++;
-                } else if (c == 's' || c == 'S') {
-                    sCount++;
+        int days = size / 1440;
+        int minutes = size % 1440;
+        int counter = 0;
+        int counterDay = 0;
+        for (int i = 0; i < 1440; i++) {
+            if (isArithmetic(i)) {
+                if (i < minutes) {
+                    counter ++;
                 }
+                counterDay ++;
             }
         }
-        System.out.println("");
-        if (tCount > sCount) {
-            System.out.println("English");
-        } else if (sCount > tCount) {
-            System.out.println("French");
-        } else {
-            System.out.println("English");
+        int total = days * counterDay + counter;
+        System.out.println(total);
+    }
+
+    // starting from 12
+    // after i minutes
+    // is this an arithmetic time?
+    private static boolean isArithmetic(int minutes) {
+        int time = (720 + minutes) % 1440;
+        int hr = time / 60;
+        int m = time % 60;
+        List<Integer> list = new ArrayList<>();
+        if (hr > 9) {
+            list.add(hr/10);
         }
+        list.add(hr %10);
+        list.add(m/10);
+        list.add(m%10);
+        int diff = Integer.MAX_VALUE;
+        for(int i = 0; i< list.size() -1; i++) {
+            int newDiff = list.get(i + 1) - list.get(i);
+            if(i == 0) {
+                diff = newDiff;
+            } else if(newDiff != diff) {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ CCC-things/src/Main22.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main22 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int tCount = 0;
+        int sCount = 0;
+        for (int i = 0; i < size; i++) {
+            String line = sc.nextLine();
+            for (char c : line.toCharArray()) {
+                if (c == 't' || c == 'T') {
+                    tCount++;
+                } else if (c == 's' || c == 'S') {
+                    sCount++;
+                }
+            }
+        }
+        System.out.println("");
+        if (tCount > sCount) {
+            System.out.println("English");
+        } else if (sCount > tCount) {
+            System.out.println("French");
+        } else {
+            System.out.println("English");
+        }
+    }
+}
