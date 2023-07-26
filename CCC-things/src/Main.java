2312b07f7dc09c203a9bcafe6d6f1e8229c8720f
Kaden Zhang <zhangwkaden@gmail.com>
2023-07-25 20:31:05 -0400

    2015 ccc

--- CCC-things/src/Main.java
+++ CCC-things/src/Main.java
@@ -1,35 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] input = sc.next().toCharArray();
-        int hFlips = 0;
-        int vFlips = 0;
-
-        for(int i = 0; i < input.length; i++){
-        if(input[i] == 'V'){
-            vFlips++;
-        }
-        else{
-            hFlips++;
-        }
-        }
-        hFlips = hFlips % 2;
-        vFlips = vFlips % 2;
-        if(hFlips == 0 && vFlips == 0){
-            System.out.println("1 2");
-            System.out.println("3 4");
-        } else if (hFlips == 1 && vFlips == 0) {
-            System.out.println("3 4");
-            System.out.println("1 2");
-        } else if (hFlips == 0 && vFlips == 1) {
-            System.out.println("2 1");
-            System.out.println("4 3");
-        } else {
-            System.out.println("4 3");
-            System.out.println("2 1");
+        int size = sc.nextInt();
+        int output = 0;
+        boolean ignored = false;
+        int[] numbers = new int[size];
+        int ignoreCounter = 0;
+        int arrayCount = 0;
+        for (int i = 0; i < size; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                ignored = true;
+            }
+                if (!ignored) {
+                    numbers[arrayCount] = num;
+                    arrayCount++;
+                } else {
+                    ignoreCounter++;
+                    numbers[i - ignoreCounter] = 0;
+                    ignoreCounter++;
+                    ignored = false;
+                    arrayCount--;
+                }
         }
 
+        for(int i = 0; i < size; i++){
+            output += numbers[i];
+        }
+        System.out.println(output);
     }
 }
