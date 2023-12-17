e9a166e4f291720160ef25c07e10a09defaac3ac
Kaden Zhang <zhangwkaden@gmail.com>
2023-12-17 10:52:31 -0500

    contest

--- CCC-things/src/Main.java
+++ CCC-things/src/Main.java
@@ -1,34 +1,56 @@
-import java.util.*;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int output = 0;
-        boolean ignored = false;
-        int[] numbers = new int[size];
-        int ignoreCounter = 0;
-        int arrayCount = 0;
-        for (int i = 0; i < size; i++) {
-            int num = sc.nextInt();
-            if (num == 0) {
-                ignored = true;
-            }
-                if (!ignored) {
-                    numbers[arrayCount] = num;
-                    arrayCount++;
-                } else {
-                    ignoreCounter++;
-                    numbers[i - ignoreCounter] = 0;
-                    ignoreCounter++;
-                    ignored = false;
-                    arrayCount--;
-                }
+        int cows = sc.nextInt();
+        int canes = sc.nextInt();
+        long[] cowHeights = new long[cows];
+        long[] canesHeight = new long[canes];
+        long[] outputs;
+
+        for (int i = 0; i < cows; i++){
+            cowHeights[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < size; i++){
-            output += numbers[i];
+        for (int i = 0; i < canes; i++){
+            canesHeight[i] = sc.nextInt();
         }
-        System.out.println(output);
+
+        outputs = cowHeights;
+
+        if(canes != 0) {
+            for (int i = 0; i < canes; i++) {
+                long alreadyEaten = 0;
+
+                for (int j = 0; j < cows; j++) {
+                    if (alreadyEaten == canesHeight[i]) {
+                        break;
+                    }
+                    if (cowHeights[j] <= canesHeight[i]) {
+                        if (cowHeights[j] - alreadyEaten > 0) {
+                            long ae = cowHeights[j] - alreadyEaten;
+                            outputs[j] += ae;
+                            alreadyEaten += ae;
+                        }
+                    } else if (cowHeights[j] > canesHeight[i]) {
+                        outputs[j] += canesHeight[i] - alreadyEaten;
+                        canesHeight[i] = 0;
+                    }
+                }
+            }
+        }
+        for (long cowHeight : cowHeights) {
+            System.out.println(cowHeight);
+        }
     }
 }
+/*
+SAMPLE INPUT:
+3 2
+3 2 5
+6 1
+SAMPLE OUTPUT:
+7
+2
+7
+ */
---
+++ CCC-things/src/Main31.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int output = 0;
+        boolean ignored = false;
+        int[] numbers = new int[size];
+        int ignoreCounter = 0;
+        int arrayCount = 0;
+        for (int i = 0; i < size; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                ignored = true;
+            }
+                if (!ignored) {
+                    numbers[arrayCount] = num;
+                    arrayCount++;
+                } else {
+                    ignoreCounter++;
+                    numbers[i - ignoreCounter] = 0;
+                    ignoreCounter++;
+                    ignored = false;
+                    arrayCount--;
+                }
+        }
+
+        for(int i = 0; i < size; i++){
+            output += numbers[i];
+        }
+        System.out.println(output);
+    }
+}
